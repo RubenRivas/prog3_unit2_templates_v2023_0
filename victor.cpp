10f90f2a94fdd4f6b653b44486b3459c55598777
RubenRivas <rudrivas@hotmail.com>
2023-01-17 11:11:44 -0500

    Initial commit

--- victor.cpp
+++ victor.cpp
@@ -10,35 +10,30 @@ victor::victor(int size) {
 
 }
 
-victor::victor(const victor &other) { // Copiar el contenido de otro en el actual (this)
-   // Copio el tamaño (sz_)
-   sz_ = other.sz_;
-   // Copia del contenido de data
-   data_ = new int[sz_];
-   copy(other.data_, other.data_ + sz_, data_);
+victor::victor(const victor &other):
+    sz_(other.sz_),                 // Copio el tamaño (sz_)
+    data_(new int[other.sz_]) {     // Reservar el espacio
+    // Copiar contenido
+    copy(other.data_, other.data_ + sz_, data_);
 }
 
-victor::victor(victor &&other) {  // Mover el contenido de otro en el actual (this)
-   sz_ = other.sz_;
-   data_ = other.data_;  // Copia la referencia en el nuevo victor
-   // Deshabilitacion de other
-   other.sz_ = 0;
-   other.data_ = nullptr;
+victor::victor(victor &&other):
+    sz_(other.sz_),                 // Copio el tamaño (sz_)
+    data_(other.data_) {            // Mover el contenido
+    // Deshabilitacion de other
+    other.sz_ = 0;
+    other.data_ = nullptr;
 }
 
 victor &victor::operator=(const victor &other) {
     if (this == &other) return *this;
-
     // Liberar memoria anterior
     delete [] data_;
-
     // Copio el tamaño (sz_)
     sz_ = other.sz_;
-
     // Copia del contenido de data
     data_ = new int[sz_];
     copy(other.data_, other.data_ + sz_, data_);
-
     return *this;
 }
 
